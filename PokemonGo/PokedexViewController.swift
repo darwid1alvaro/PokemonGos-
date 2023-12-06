755ef6471477fabef7292ae466667649ff04b5ee
darwid <levochima@gmail.com>
2023-12-05 19:49:19 -0500

    Initial Commit

---
+++ PokemonGo/PokedexViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PokedexViewController.swift
+//  PokemonGo
+//
+//  Created by rigoberto on 5/12/23.
+//
+
+import UIKit
+
+class PokedexViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
